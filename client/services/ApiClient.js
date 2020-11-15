30c47b264d3c68a66ac843684836e1947f5a0141
Luis Rojas <luisrohu91@gmail.com>
2020-11-15 13:35:02 -0700

    refactored apiclient

--- client/services/ApiClient.js
+++ client/services/ApiClient.js
@@ -2,4 +2,15 @@ import axios from 'axios'
 
 const ApiClient = axios.create({ baseURL: 'http://localhost:5001/api' })
 
+ApiClient.interceptors.request.use(
+    async (config) => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (err) => Promise.reject(err)
+)
+
 export default ApiClient
